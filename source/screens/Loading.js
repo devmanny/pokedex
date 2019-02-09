88fe768e90ab7c11dad3481dfdf59c2f2e5f0196
Manuel Vargas <im@devmanny.io>
2019-02-09 14:33:26 -0600

    loading working great

--- source/screens/Loading.js
+++ source/screens/Loading.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { SafeAreaView, View, StyleSheet } from 'react-native';
 import LottieView from 'lottie-react-native';
+
+import { whiteColor } from '../util';
 import Intro from '../assets/animations/intro';
 
-const Loading = () => <LottieView resizeMode="contain" source={Intro} autoPlay loop />;
+const styles = StyleSheet.create({
+    conatiner: {
+        backgroundColor: whiteColor,
+        flex: 1,
+    },
+});
+
+const Loading = () => (
+    <SafeAreaView style={styles.conatiner}>
+        <LottieView resizeMode="contain" source={Intro} autoPlay loop />
+    </SafeAreaView>
+);
+
 export default Loading;
