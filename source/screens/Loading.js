865c8f29e5319f3017fa1335cf42e42cfcc87293
Manuel Vargas <im@devmanny.io>
2019-02-09 14:06:48 -0600

    fixed building iOS lottie

--- source/screens/Loading.js
+++ source/screens/Loading.js
@@ -2,5 +2,5 @@ import React from 'react';
 import LottieView from 'lottie-react-native';
 import Intro from '../assets/animations/intro';
 
-const Loading = () => <LottieView source={Intro} autoPlay loop />;
+const Loading = () => <LottieView resizeMode="contain" source={Intro} autoPlay loop />;
 export default Loading;
