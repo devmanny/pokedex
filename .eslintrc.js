14691dfd3fa2e4a1cef393a5d5cd464b9382b7f4
Manuel Vargas <im@devmanny.io>
2019-02-09 12:25:27 -0600

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,26 +5,18 @@ module.exports = {
         "react-native/react-native": true,
         "node": true,
         "es6": true
-    }
+    },
     "plugins": [
         "react-native",
         "react",
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true,
-            "destructuring": true,
-            "arrowFunctions": true,
-            "classes": true,
-            "objectLiteralShorthandMethods": true,
-            "spread": true
-        }
-    }
     "rules": {
         "indent": [ "error", 4 ],
         "linebreak-style": [ "error", "unix" ],
         "quotes": [ "error", "single" ],
         "semi": [ "error", "always" ],
+        "react/jsx-indent": [ "error", 4 ],
+        "react/jsx-filename-extension": 0,
         "react-native/no-unused-styles": ["error"],
         "react-native/split-platform-components": ["error"],
         "react-native/no-inline-styles": ["error"],
