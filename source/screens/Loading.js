762bdfbb922ab4a242ea7c0ff2f5e6c82e02ab88
Manuel Vargas <im@devmanny.io>
2019-02-10 09:39:47 -0600

    I am using lodash to get variables nested in an object in a secure way

--- source/screens/Loading.js
+++ source/screens/Loading.js
@@ -1,8 +1,12 @@
 import React, { PureComponent } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
-import { func, arrayOf } from 'prop-types';
+import {
+    func, shape, string, arrayOf,
+} from 'prop-types';
 import LottieView from 'lottie-react-native';
+import lodash from 'lodash';
+import { NavigationActions } from 'react-navigation';
 
 import { whiteColor, indexing } from '../util';
 import Intro from '../assets/animations/intro';
@@ -18,15 +22,25 @@ const styles = StyleSheet.create({
 class Loading extends PureComponent {
     constructor(props) {
         super(props);
+        this.getPokemonList();
+    }
 
-        const { pokemonList, setPokemonList } = props;
+    getPokemonList = () => {
+        const { goToHome, pokemonList, setPokemonList } = this.props;
 
         if (pokemonList.length === 0) {
             request.get('/pokemon/?offset=0&limit=5000')
                 .then((response) => {
+                    // this pattern is to get the pokemon ID of the URL of its image
                     const pokemonPattern = /https:\/\/pokeapi\.co\/api\/v2\/pokemon\/(\d+)\//;
+                    const securePokemonListFromRequest = lodash.get(response, 'data.results', []);
 
-                    const responseList = response.data.map((pokemon) => {
+                    if (securePokemonListFromRequest.length === 0) {
+                        setTimeout(() => goToHome(), 1000);
+                        return;
+                    }
+
+                    const responseList = securePokemonListFromRequest.map((pokemon) => {
                         const id = pokemonPattern.exec(pokemon.url);
 
                         if (id) {
@@ -39,20 +53,25 @@ class Loading extends PureComponent {
                         return pokemon;
                     });
 
-                    const pokemons = indexing(responseList, 'id');
+                    const indexedPokemonList = indexing(responseList, 'id');
 
-                    setPokemonList(pokemons);
+                    setPokemonList({
+                        raw: responseList,
+                        pokemons: indexedPokemonList,
+                    });
                 })
                 .catch((err) => {
-                    console.warn(err);
+                    console.warn(err.toString());
                 });
+        } else {
+            setTimeout(() => goToHome(), 3000);
         }
     }
 
     render() {
         return (
             <SafeAreaView style={styles.conatiner}>
-                <LottieView resizeMode="contain" source={Intro} autoPlay />
+                <LottieView resizeMode="contain" source={Intro} autoPlay loop={false} />
             </SafeAreaView>
         );
     }
@@ -61,16 +80,26 @@ class Loading extends PureComponent {
 Loading.defaultProps = {
     pokemonList: [],
     setPokemonList: () => {},
+    goToHome: () => {},
 };
 
 Loading.propTypes = {
+    goToHome: func,
     setPokemonList: func,
-    pokemonList: arrayOf,
+    pokemonList: arrayOf(
+        shape({
+            name: string,
+            url: string,
+        }),
+    ),
 };
 
-const mapStateToProps = state => ({
-    pokemonList: state.pokemons.list,
-});
+const mapStateToProps = (state) => {
+    console.warn(state.pokemons);
+    return ({
+        pokemonList: state.pokemons.list,
+    });
+};
 
 const mapDispatchToProps = dispatch => ({
     setPokemonList: (pokemons) => {
@@ -78,6 +107,13 @@ const mapDispatchToProps = dispatch => ({
             type: 'SET_POKEMON_LIST',
             payload: pokemons,
         });
+    },
+    goToHome: () => {
+        dispatch(
+            NavigationActions.navigate({
+                routeName: 'Home',
+            }),
+        );
     },
 });
 
